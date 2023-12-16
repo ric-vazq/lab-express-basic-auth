bc84fe162946359e980bd85ee8570ace115f7278
Ricardo Vazquez <rickyrabv@gmail.com>
2023-12-16 14:39:07 +0100

    iteration 2 and 3 complete

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,12 +2,14 @@ const router = require("express").Router();
 const User = require("../models/User.model");
 const bcrypt = require("bcrypt");
 const saltRounds = 12; 
+const { isLoggedIn, isLoggedOut } = require('../middleware/route-guard');
 
 /* GET home page */
-router.get("/signup", (req, res, next) => {
+router.get("/signup", isLoggedOut, (req, res, next) => {
   res.render("auth/signup");
 });
-router.post("/signup", (req, res, next) => {
+
+router.post("/signup", isLoggedOut, (req, res, next) => {
     const { username, password } = req.body;
 
     User.findOne({username})
@@ -27,6 +29,43 @@ router.post("/signup", (req, res, next) => {
      })
      .catch(err => next(err))
         }
+    })
+});
+
+router.get("/login", isLoggedOut, (req, res, next) => {
+    res.render("auth/login")
+});
+
+router.post("/login", isLoggedOut, (req, res, next) => {
+    console.log('SESSION =====> ', req.session);
+    const { username, password } = req.body; 
+
+    if (username === '' || password === ''){
+        res.render('auth/login', {error: 'Please enter both, username and passord to login'});
+        return; 
+    }
+
+    User.findOne({ username })
+    .then(user => {
+        if (!user){
+            res.render('auth/login', {error: 'User not found and/or incorrect password'})
+            return
+        } else if (bcrypt.compare(password, user.password)){
+            req.session.currentUser = user;
+            res.redirect('/profile')
+        } else {
+            res.render('auth/login', {error: 'User not found and/or incorrect password'})
+        }
+    })
+    .catch(err => next(err));
+});
+router.get("/profile", isLoggedIn, (req, res, next) => {
+    res.render('user/profile', { userInSession: req.session.currentUser })
+});
+router.post('/logout', isLoggedIn, (req, res, next) => {
+    req.session.destroy(err => {
+        if (err) next(err);
+        res.redirect('/')
     })
 })
 
